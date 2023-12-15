33a2c153e264ffb973109847bae686cfcb41a84e
Dr. Konstantin Shmirko <shmirko.konstantin@gmail.com>
2023-12-15 12:27:04 +0000

    Small fixtures

--- src/db/database.js
+++ src/db/database.js
@@ -6,20 +6,47 @@ class Database{
         this.db = require('rethinkdbdash')(options)
     }
 
+    /**
+     * 
+     * @returns array of tables in Lidar database
+     */
     async tables(){
         return this.db.tableList().run()
     }
 
-    async findExperimentById(id){
-        return this.db.table('Experiments').get(id).run()
+    /**
+     * 
+     * @param {string} exp_id - primary key of experiment.
+     * @returns Experiment document for the specific pk
+     */
+    async findExperimentById(exp_id){
+        return this.db.table('Experiments').get(exp_id).run()
     }
 
+    /**
+     * 
+     * @returns list of all experiments
+     */
     async findAllExperiments(){
         return this.db.table('Experiments').run()
     }
 
+    /**
+     * 
+     * @param {string} expid - id of experiment
+     * @returns list of measurements for the specific experiment id.
+     */
     async findMeasurementsByExperimentId(expid){
         return this.db.table('Measurements').filter({exp_id:expid}).run()
+    }
+
+    /**
+     * 
+     * @param {string} meas_id id of the specific measurement
+     * @returns certain Measurement for the given pk
+     */
+    async findMeasurementById(meas_id){
+        return this.db.table('Measurements').get(meas_id).run()
     }
 }
 
--- src/routes/experiments-route.js
+++ src/routes/experiments-route.js
@@ -1,13 +1,22 @@
 module.exports  =  function (fastify, opts, done) {
     
-    fastify.get('/tables/:id',async (request, reply)=>{
-    
-        const {id} = request.params
+    fastify.get('/tables',async (request, reply)=>{    
         let tbls =  await fastify.rethinkdb.tables()
-        //console.log(Object.keys(tbls))
-        console.log(tbls)
-        
         return tbls
+    })
+
+    fastify.get('/experiments', async (req, rep)=>{
+        return await fastify.rethinkdb.findAllExperiments()
+    })
+
+    fastify.get('/experiments/:id', async(req, rep)=>{
+        const {id} = req.params
+        console.log(id)
+        return await fastify.rethinkdb.findExperimentById(id)
+    })
+
+    fastify.post('/experiments', async (req, rep)=>{
+        
     })
     done()
 }
